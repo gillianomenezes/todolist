46e4631bcd15f73e0eb6549983d07db0e68e17c2
G. Silva De Menezes <g.silva.de.menezes@avanade.com>
2018-04-25 10:04:01 -0300

    Testing getJSON

--- todolist.js
+++ todolist.js
@@ -1,5 +1,26 @@
-var main = function(){
+var main = function(todosObjects){
 	"user strict";
+
+	var todoTags = [];
+	var todoByTags = [];
+
+	todosObjects.map(function(todoObject){
+		todoObject.tags.forEach(function(tag){
+			if(todoTags.indexOf(tag) === -1){
+				todoTags.push(tag);
+			}
+		});
+	});
+
+	todoTags.map(function(tag){
+		var descriptions = [];
+		todosObjects.forEach(function(todoObject){
+			if(todoObject.tags.indexOf(tag) !== -1){
+				descriptions.push(todoObject.description);
+			}
+		});
+		todoByTags.push({ "tag":tag, "descriptions":descriptions });
+	});
 
 	$('.tabs a span').toArray().forEach(function(element) {
 		$(element).on('click', function(){
@@ -13,11 +34,29 @@ var main = function(){
 
 			} else if($element.parent().is(':nth-child(2)')) {
 
-			} else if($element.parent().is(':neth-child(3)')){
+			} else if($element.parent().is(':nth-child(3)')){
 
+			} else if($element.parent().is(':nth-child(4)')){
+
+				todoByTags.forEach(function(todoByTag){
+					var $tagLabel = $('<label>').text(todoByTag.tag);
+					var $decriptions = $('<ul>');
+					todoByTag.descriptions.forEach(function(description){
+						$decriptions.append($('<li>').text(description));
+					});
+					$('.content').append($tagLabel);
+					$('.content').append($decriptions);
+				});
 			}
 
 			return false;
 		});
 	});
 }
+
+$(document).ready(function(){
+
+	$.getJSON('todos.json', function(todosObjects){
+		main(todosObjects);
+	});
+});
